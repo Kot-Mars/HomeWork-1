ac2ee256e3b0fb133d5148ca5e805dc2fac322b4
Kot-Mars <61290390+Kot-Mars@users.noreply.github.com>
2023-03-10 16:49:30 +0300

    Main 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Easy
-        Easy easy = new Easy();
-        easy.SumRes();
-        //Medium
-        Medium medium = new Medium();
-        medium.Multiplication(2);
-        medium.Multiplication(5, 2.5);
-        medium.Multiplication(2, 5, 2 );
-        //Hard
-        Hard hard = new Hard();
-        hard.RandomNum(0);
+
+        Scanner num = new Scanner(System.in);
+        System.out.println("Выберите команду:\n 1 - Easy\n 2 - Medium\n 3 - Hard");
+        if(num.hasNextInt()) {
+            switch (num.nextInt()) {
+                case 1:
+                    //Easy
+                    Easy easy = new Easy();
+                    easy.SumRes();
+                    break;
+                case 2:
+                    //Medium
+                    Medium medium = new Medium();
+                    medium.Multiplication(2);
+                    medium.Multiplication(5, 2.5);
+                    medium.Multiplication(2, 5, 2 );
+                    break;
+                case 3:
+                    //Hard
+                    Hard hard = new Hard();
+                    hard.RandomNum(0);
+                    break;
+                default:
+                    System.out.println("Непонял команду! Повторите попытку");
+                    break;
+            }
+        }
+        else {
+            System.out.println("Ошибка! Введите число!");
+        }
     }
 }
